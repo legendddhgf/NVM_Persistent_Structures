6f4795e50ffc3fce6b710f3d08d432a2e7ba66c4
legendddhgf <legendddhgf@gmail.com>
2017-12-29 00:08:22 -0800

    did some reading, started on pwrapper

--- src/Common.c
+++ src/Common.c
@@ -1,5 +1,38 @@
 #include "Common.h"
 
+/* TODO
+static PMEMobjpool *pop;
+
+void pmemInit() {
+
+  const char *path = "/mnt/pmemdir";
+
+  pop = NULL;
+
+  if (file_exists(path) != 0) {
+    if ((pop = pmemobj_create(path, POBJ_LAYOUT_NAME(array),
+            PMEMOBJ_MIN_POOL, CREATE_MODE_RW)) == NULL) {
+      printf("failed to create pool\n");
+      return 1;
+    }
+  } else {
+    if ((pop = pmemobj_open(path, POBJ_LAYOUT_NAME(array)))
+        == NULL) {
+      printf("failed to open pool\n");
+      return 1;
+    }
+  }
+}
+
+void *pmemMalloc() {
+
+}
+
+void pmemFree() {
+
+}
+*/
+
 // since Generic is using unsigned integers, hash function just returns their
 // value
 uint32_t GenericHash(Generic data) {
